a031417710d3f812b86c218196d9608af874d534
danielperesaraujo <daniperesaraujo17@gmail.com>
2025-06-26 20:48:58 -0300

    condições

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 67
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if (vel > 60) {
+    console.log(`VocÊ ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
