5b7348ecbb04ab8059604ae0c26ae2f08225e204
danielperesaraujo <daniperesaraujo17@gmail.com>
2025-07-03 22:31:37 -0300

    Primeiro exercicio JavaScript

--- aula13/ex014/script.js
+++ aula13/ex014/script.js
@@ -3,4 +3,19 @@ function carregar() {
     var img = window.document.getElementById('imagem')
     var data = new Date()
     var hora = data.getHours()
+    //var hora = 16
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //bom dia
+        img.src = 'manha.png'
+        document.body.style.background = '#fbdcae'
+    } else if (hora >= 12 && hora < 18) {
+        //boa tarde
+        img.src = 'tarde.png'
+        document.body.style.background = '#38b6ff'
+    }else {
+        //boa noite
+        img.src = 'noite.png'
+        document.body.style.background = '#000735'
+    }
 }
